0eb62577401c73f888dd62910503704659e94275
Dmitry Borko <dmitry_borko@UBUNTU.myguest.virtualbox.org>
2024-10-24 21:25:08 +0300

    Updated FuncA to calculate sum of series

--- FuncA.cpp
+++ FuncA.cpp
@@ -4,6 +4,25 @@
 FuncA::FuncA(){
 }
 
-double FuncA::Calculate(){
-return std::sin(3.14);
+double FuncA::Calculate(double x, int terms){
+double sum = 0.0;
+
+    for (int n = 0; n < terms; ++n) {
+        // Обчислюємо (-1)^n
+        double sign = pow(-1, n);
+        
+        // Обчислюємо (2n)!
+        double numerator = tgamma(2 * n + 1); // tgamma(n+1) = n!
+
+        // Обчислюємо (n!)^2
+        double denominator = pow(tgamma(n + 1), 2);
+
+        // Член ряду
+        double term = (sign * numerator * pow(x, n)) / (pow(4, n) * denominator);
+
+        // Додаємо до суми
+        sum += term;
+    }
+
+    return sum;
 }
