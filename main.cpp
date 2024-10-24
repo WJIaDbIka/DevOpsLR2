6d8dbc1166339d5c1c4bb2e6cf28717c0b6fb1b8
Dmitry Borko <dmitry_borko@UBUNTU.myguest.virtualbox.org>
2024-10-24 21:45:02 +0300

    Hotfix: Calculate sum of first 3 elements

--- main.cpp
+++ main.cpp
@@ -1,9 +1,18 @@
 #include <iostream>
 #include "FuncA.h"
 
-int main(){
-	FuncA func;
-	std::cout<<"Result: " << func.Calculate() <<"\n";
-	return 0;
+int main() {
+    double x;
+    int terms;
 
+    std::cout << "Введіть значення x: ";
+    std::cin >> x;
+    terms = 3;
+
+    FuncA func; // Створюємо об'єкт класу FuncA
+    double result = func.Calculate(x, terms); // Викликаємо метод через об'єкт
+    std::cout << "Сума ряду: " << result << std::endl;
+
+    return 0;
 }
+
